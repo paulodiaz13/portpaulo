f32475737e8ec8a10ebbcba823c967f6d30a84b2
Paulo Cesar Diaz Solarte <paulocesardiaz13@gmail.com>
2023-01-29 13:38:21 -0500

    Paulo
    first commit

---
+++ src/components/home/HeaderSocials.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { FaGithub, FaFacebookF, FaLinkedinIn, FaTwitter, FaDev } from 'react-icons/fa';
+import { SiHashnode } from 'react-icons/si'
+
+const HeaderSocials = () => {
+    return (
+        <div className='home__socials'>
+            <a href='https://github.com/paulodiaz13' className='home__social-link' target='_blank' rel='noreferrer'>
+                <FaGithub />
+            </a>
+
+            <a href='https://hashnode.com/' className='home__social-link' target='_blank' rel='noreferrer'>
+                <SiHashnode />
+            </a>
+
+            <a href='https://dev.to/' className='home__social-link' target='_blank' rel='noreferrer'>
+                <FaDev />
+            </a>
+
+            <a href='https://www.linkedin.com/in/paulocesardiaz' className='home__social-link' target='_blank' rel='noreferrer'>
+                <FaLinkedinIn />
+            </a>
+
+            <a href='https://wwww.facebook.com/paulo.solarte' className='home__social-link' target='_blank' rel='noreferrer'>
+                <FaFacebookF />
+            </a>
+
+            <a href='https://www.twitter.com/' className='home__social-link' target='__blank' rel='noreferrer'>
+                <FaTwitter />
+            </a>
+        </div>
+    );
+};
+
+export default HeaderSocials;
